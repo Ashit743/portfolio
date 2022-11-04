473c877a7377be456f6d530812ba1bd703565b6b
Ashit Darurmath <ashitsdarurmath@gmail.com>
2022-11-05 03:10:27 +0530

    add basic route to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Proj1Component } from './projects/proj1/proj1.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:WelcomeComponent
+  },
+  {
+    path:'proj1',component:Proj1Component
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
